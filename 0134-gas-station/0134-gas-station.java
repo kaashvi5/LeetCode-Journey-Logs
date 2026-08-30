54cc2f253fe1058ca49ee91ce97c12f9e332aa5f
Kaashvi Gupta <kaashvi.codes@gmail.com>
2026-08-30 11:24:24 +0530

    Time: 2 ms (99.2%), Space: 119.8 MB (10.28%) - LeetHub

---
+++ 0134-gas-station/0134-gas-station.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int totalSurplus = 0;
+        int surplus = 0;
+        int start = 0;
+        
+        for (int i = 0; i < gas.length; i++) {
+            int diff = gas[i] - cost[i];
+            totalSurplus += diff;
+            surplus += diff;
+            
+            if (surplus < 0) {
+                start = i + 1;
+                surplus = 0;
+            }
+        }
+        
+        return totalSurplus >= 0 ? start : -1;
+    }
+}
