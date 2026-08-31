e2733f0f404f6cda89e699b54d2c09d65240d829
Kaashvi Gupta <kaashvi.codes@gmail.com>
2026-08-31 10:19:58 +0530

    Time: 0 ms (100%), Space: 41.9 MB (94.63%) - LeetHub

--- 1922-count-good-numbers/1922-count-good-numbers.java
+++ 1922-count-good-numbers/1922-count-good-numbers.java
@@ -6,26 +6,21 @@ class Solution {
         long even = (n + 1) / 2;
         long odd = n / 2;
 
-        long ans = (power(5, even) * power(4, odd)) % MOD;
+        long ans = (power(5, even)%MOD * power(4, odd)%MOD) % MOD;
 
         return (int) ans;
     }
 
     private long power(long base, long exponent) {
 
-        long result = 1;
-
-        while (exponent > 0) {
-
-            // If exponent is odd
-            if (exponent % 2 == 1) {
-                result = (result * base) % MOD;
-            }
-
-            base = (base * base) % MOD;
-            exponent /= 2;
+        if(exponent==0){
+            return 1;
         }
 
-        return result;
+        //long half=power(base,exponent/2);
+        if(exponent%2==0){
+            return  power((base*base)%MOD,exponent/2)%MOD;
+        }
+        return (base*power(base,exponent-1))%MOD;
     }
 }
