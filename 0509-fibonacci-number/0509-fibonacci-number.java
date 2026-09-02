85ad5e2f112e2b48677eba7d6b5720c7914eecd2
Kaashvi Gupta <kaashvi.codes@gmail.com>
2026-09-02 09:51:06 +0530

    Time: 9 ms (42.7%), Space: 41.4 MB (99.07%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int fib(int n) {
+        return fibo(n);
+    }
+    private int fibo(int n){
+       if(n<=1){
+        return n;
+       }
+       return fibo(n-1)+fibo(n-2);
+    }
+}
