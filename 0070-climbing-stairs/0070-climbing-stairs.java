ca1fc65dd50b092ff71c931827c81a46423e7f94
Kaashvi Gupta <kaashvi.codes@gmail.com>
2026-09-02 10:38:35 +0530

    Time: 0 ms (100%), Space: 42.1 MB (36.06%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -2,15 +2,15 @@ class Solution {
     public int climbStairs(int n) {
         if (n <= 2) return n;
         
-        int prev2 = 1; // ways to reach step 1
-        int prev1 = 2; // ways to reach step 2
+        int a = 1; // ways to reach step 1
+        int b = 2; // ways to reach step 2
         
         for (int i = 3; i <= n; i++) {
-            int curr = prev1 + prev2;
-            prev2 = prev1;
-            prev1 = curr;
+            int c = b + a;
+            a = b;
+            b = c;
         }
         
-        return prev1;
+        return b;
     }
 }
