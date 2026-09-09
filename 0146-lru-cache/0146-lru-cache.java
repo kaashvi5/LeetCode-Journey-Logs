d501c380bce200778bd49424b7f3ecb50eb828f4
Kaashvi Gupta <kaashvi.codes@gmail.com>
2026-09-09 08:38:30 +0530

    Time: 14 ms (96.04%), Space: 58.8 MB (81.02%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.java
@@ -0,0 +1,89 @@
+import java.util.HashMap;
+
+class LRUCache {
+
+    class Node {
+        int key;
+        int value;
+        Node prev, next;
+
+        Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private int capacity;
+    private HashMap<Integer, Node> map;
+    private Node head, tail;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        map = new HashMap<>();
+
+        // Dummy nodes
+        head = new Node(0, 0);
+        tail = new Node(0, 0);
+
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    // Remove a node from the list
+    private void remove(Node node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    // Add node right after head
+    private void add(Node node) {
+        node.next = head.next;
+        node.prev = head;
+
+        head.next.prev = node;
+        head.next = node;
+    }
+
+    public int get(int key) {
+        if (!map.containsKey(key)) {
+            return -1;
+        }
+
+        Node node = map.get(key);
+
+        // Make it most recently used
+        remove(node);
+        add(node);
+
+        return node.value;
+    }
+
+    public void put(int key, int value) {
+
+        // Key already exists
+        if (map.containsKey(key)) {
+            Node node = map.get(key);
+
+            node.value = value;
+
+            remove(node);
+            add(node);
+
+            return;
+        }
+
+        // Create new node
+        Node node = new Node(key, value);
+
+        map.put(key, node);
+        add(node);
+
+        // Capacity exceeded
+        if (map.size() > capacity) {
+            Node lru = tail.prev;
+
+            remove(lru);
+            map.remove(lru.key);
+        }
+    }
+}
